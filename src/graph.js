eeccbb0776e952661820be64a6f7482d5d6fc017
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-05-17 22:58:33 -0300

    fixing exports of graph.js and queue.js

--- src/graph.js
+++ src/graph.js
@@ -1,96 +1,88 @@
+const Queue = require('./queue');
 // create a graph class
 class Graph {
-	// defining vertex array and
-	// adjacent list
-	constructor(noOfVertices)
-	{
-		this.noOfVertices = noOfVertices;
-		this.AdjList = new Map();
-	}
+  // defining vertex array and
+  // adjacent list
+  constructor(noOfVertices) {
+    this.noOfVertices = noOfVertices;
+    this.AdjList = new Map();
+  }
 
-	// functions to be implemented
+  // functions to be implemented
 
-	addVertex(v)
-    {
-        // initialize the adjacent list with a
-        // null array
-        this.AdjList.set(v, []);
-    }
-	
-    // add edge to the graph
-    addEdge(v, w)
-    {
-        // get the list for vertex v and put the
-        // vertex w denoting edge between v and w
-        this.AdjList.get(v).push(w);
+  addVertex(v) {
+    // initialize the adjacent list with a
+    // null array
+    this.AdjList.set(v, []);
+  }
 
-        // Since graph is undirected,
-        // add an edge from w to v also
-        this.AdjList.get(w).push(v);
-    }
+  // add edge to the graph
+  addEdge(v, w) {
+    // get the list for vertex v and put the
+    // vertex w denoting edge between v and w
+    this.AdjList.get(v).push(w);
 
-	// printGraph()
+    // Since graph is undirected,
+    // add an edge from w to v also
+    this.AdjList.get(w).push(v);
+  }
 
-	
-    // function to performs BFS
-    bfs(startingNode)
-    {
-    
-        // create a visited object
-        let visited = {};
-    
-        // Create an object for queue
-        let q = new Queue();
-    
-        // add the starting node to the queue
-        visited[startingNode] = true;
-        q.enqueue(startingNode);
-    
-        // loop until queue is empty
-        while (!q.isEmpty()) {
-            // get the element from the queue
-            let getQueueElement = q.dequeue();
-    
-            // passing the current vertex to callback function
-            console.log(getQueueElement);
-    
-            // get the adjacent list for current vertex
-            let get_List = this.AdjList.get(getQueueElement);
-    
-            // loop through the list and add the element to the
-            // queue if it is not processed yet
-            for (let i in get_List) {
-                let neigh = get_List[i];
-    
-                if (!visited[neigh]) {
-                    visited[neigh] = true;
-                    q.enqueue(neigh);
-                }
-            }
+  // printGraph()
+
+  // function to performs BFS
+  bfs(startingNode) {
+    // create a visited object
+    let visited = {};
+
+    // Create an object for queue
+    let q = new Queue();
+
+    // add the starting node to the queue
+    visited[startingNode] = true;
+    q.enqueue(startingNode);
+
+    // loop until queue is empty
+    while (!q.isEmpty()) {
+      // get the element from the queue
+      let getQueueElement = q.dequeue();
+
+      // passing the current vertex to callback function
+      console.log(getQueueElement);
+
+      // get the adjacent list for current vertex
+      let get_List = this.AdjList.get(getQueueElement);
+
+      // loop through the list and add the element to the
+      // queue if it is not processed yet
+      for (let i in get_List) {
+        let neigh = get_List[i];
+
+        if (!visited[neigh]) {
+          visited[neigh] = true;
+          q.enqueue(neigh);
         }
+      }
     }
-    printGraph()
-    {
-        // get all the vertices
-        let get_keys = this.AdjList.keys();
-    
-        // iterate over the vertices
-        for (let i of get_keys)
-    {
-            // great the corresponding adjacency list
-            // for the vertex
-            let get_values = this.AdjList.get(i);
-            let conc = "";
-    
-            // iterate over the adjacency list
-            // concatenate the values into a string
-            for (let j of get_values)
-                conc += j + " ";
-    
-            // print the vertex and its adjacency list
-            console.log(i + " -> " + conc);
-        }
+  }
+  printGraph() {
+    // get all the vertices
+    let get_keys = this.AdjList.keys();
+
+    // iterate over the vertices
+    for (let i of get_keys) {
+      // great the corresponding adjacency list
+      // for the vertex
+      let get_values = this.AdjList.get(i);
+      let conc = "";
+
+      // iterate over the adjacency list
+      // concatenate the values into a string
+      for (let j of get_values) conc += j + " ";
+
+      // print the vertex and its adjacency list
+      console.log(i + " -> " + conc);
     }
+  }
 }
 
 module.exports = Graph;
